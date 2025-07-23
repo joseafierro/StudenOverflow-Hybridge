f2dccd6665c49224a6f738f75852a90af9016ec2
José Fierro <jose_fierro@outlook.com>
2025-07-23 17:07:31 -0600

    Create wsgi.py

---
+++ studentoverflow/wsgi.py
@@ -0,0 +1,12 @@
+"""
+WSGI config for studentoverflow project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+"""
+
+import os
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'studentoverflow.settings')
+
+application = get_wsgi_application()
